94699a5478afd68169a4248bd5b8fcd51e251679
Aefrly <fraley.allison@ic.edu>
2026-02-23 12:18:19 -0600

    Updatad my get command

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ app.use(express.json());
 
 // Connect to database 
 const db = new sqlite3.Database('./database/inventory.db');
+
+// Get all products
+app.get('/api/products', (reg, res) => { 
+    db.all('SELECT * FROM products', (err, rows) => { 
+        res.json(rows); 
+    });
+});
