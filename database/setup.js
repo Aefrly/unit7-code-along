7a5cdad313ca2c1af7cf1d5bdc26d193e1eb6b58
Aefrly <fraley.allison@ic.edu>
2026-02-23 08:34:08 -0600

    Import the sqlite package and create a new database file

---
+++ database/setup.js
@@ -0,0 +1,5 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// Create/connect to database file
+const db = new sqlite3.Database('./database/inventory.db');
+console.log('Connected to SQLite database');
